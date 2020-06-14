45ab215d20778dd4b4045a0826ccde4a31606cb7
Ignacio Del Busto <gidelbusto@gmail.com>
2020-06-14 03:54:32 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,7 +14,9 @@ end
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kinds_name, age|
     if age <= 12
-    puts "Happy Birthday #{kinds_name}! You are now #{age} years old!"
+      puts "Happy Birthday #{kinds_name}! You are now #{age} years old!"
+    else
+      puts "You are too old for this."
    end
   end  
 end
