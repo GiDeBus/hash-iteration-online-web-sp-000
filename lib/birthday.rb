00273046a09618b68a3341549323d26314b6e27a
Ignacio Del Busto <gidelbusto@gmail.com>
2020-06-14 03:50:15 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,7 +13,7 @@ end
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kinds_name, age|
-    if 
+    if age => 12
     puts "Happy Birthday #{kinds_name}! You are now #{age} years old!"
   end  
 end
